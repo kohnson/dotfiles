907fa4c2ed72efd54040d7252dfe74263fce9676
kohnson <scp100j@gmail.com>
2021-08-15 20:55:26 -0400

    Started work on a number of things
    I have a wallpaper added now, which is nice

---
+++ install.ps1
@@ -0,0 +1,4 @@
+## Install script for my dotfiles
+
+# Wallpaper (None of the methods described by the internet will work properly on other pcs)
+Write-Host -ForegroundColor Blue "Set the wallpaper yourself, jackass"
