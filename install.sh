4f3fe7236c7fdbb75292e188496147ee66960544
kohnson <scp100j@gmail.com>
2021-08-20 09:52:12 -0400

    Improved Nitrogen
     - Added Nitrogen configs
     - Updated install.sh to match

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@
 ## To be run in "dotfiles" folder
 
 # Wallpaper
+[ ! -d /usr/share/backgrounds ] && sudo mkdir /usr/share/backgrounds # Create backgrounds folder if it doesn't exist
 sudo cp -f common/wal.jpg /usr/share/backgrounds/wal.jpg # Set wallpaper
 echo "Wallpaper written to /usr/share/backgrounds/wal.jpg"
 nitrogen --set-zoom-fill /usr/share/backgrounds/wal.jpg
-
+cp -f linux/nitrogen/* ~/.config/nitrogen/
