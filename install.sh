822aec24f46efffae0a6549e8ff620cfd930e0a8
kohnson <scp100j@gmail.com>
2021-08-20 08:38:31 -0400

    Fixed wallpaper installation
    It was previously copied to /usr/share/backgrounds as a file, it is now
    copied into the /usr/share/backgrounds directory.

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 ## To be run in "dotfiles" folder
 
 # Wallpaper
-sudo cp -f common/wal.jpg /usr/share/backgrounds # Set wallpaper
+sudo cp -f common/wal.jpg /usr/share/backgrounds/wal.jpg # Set wallpaper
 echo "Wallpaper written to /usr/share/backgrounds/wal.jpg"
 nitrogen --set-zoom-fill /usr/share/backgrounds/wal.jpg
 
