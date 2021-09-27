ccc44c3207e552320a7b08b2bf5be347ba63be3d
kohnson <scp100j@gmail.com>
2021-09-27 08:55:24 -0400

    Changed prompt style
    Main prompt changed "$ " -> "% "
    Multiline prompt changed ">>" -> "> "

--- common/pwsh/Microsoft.PowerShell_profile.ps1
+++ common/pwsh/Microsoft.PowerShell_profile.ps1
@@ -6,7 +6,7 @@ Set-PSReadlineOption -EditMode Vi -ViModeIndicator Cursor
 # Theming
 If (Get-Command Set-PoshPrompt) {
 	Set-PoshPrompt -Theme ys
-	Set-PSReadlineOption -ExtraPromptLineCount 2 -PromptText "$ "
+	Set-PSReadlineOption -ExtraPromptLineCount 1 -PromptText "% " -ContinuationPrompt "> "
 }
 # Aliases
 New-Alias -Name "g" -Value "git"
