358c19187c2a7ab246f143a453a93096362ac2b4
kohnson <scp100j@gmail.com>
2021-10-01 10:41:31 -0400

    Updated Qutebrowser config, added Qutebrowser entry to install.sh

--- install.sh
+++ install.sh
@@ -4,13 +4,16 @@
 
 # Wallpaper
 [ ! -d /usr/share/backgrounds ] && sudo mkdir /usr/share/backgrounds # Create backgrounds folder if it doesn't exist
-sudo cp -f common/wal.jpg /usr/share/backgrounds/wal.jpg # Set wallpaper
+sudo cp -f common/wal.jpg /usr/share/backgrounds/wal.jpg # Copy wallpaper
 echo "Wallpaper written to /usr/share/backgrounds/wal.jpg"
-nitrogen --set-zoom-fill /usr/share/backgrounds/wal.jpg
-cp -f linux/nitrogen/* ~/.config/nitrogen/
+nitrogen --set-zoom-fill /usr/share/backgrounds/wal.jpg # Set wallpaper
+cp -f linux/nitrogen/* ~/.config/nitrogen/ # Copy config files
 
 # ZSH
 cp -f linux/zsh/.zshrc ~/
 
 # Neovim
 cp -f common/neovim/init.vim ~/.config/nvim/
+
+# Qutebrowser
+cp -f common/qutebrowser/autoconfig.yml ~/.config/qutebrowser
