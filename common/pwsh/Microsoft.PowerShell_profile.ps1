d5b6d662577c435d0e101073a68245a165fe27ae
kohnson <scp100j@gmail.com>
2021-11-10 09:07:36 -0500

    Added note function, changed home path to $HOME

--- common/pwsh/Microsoft.PowerShell_profile.ps1
+++ common/pwsh/Microsoft.PowerShell_profile.ps1
@@ -1,17 +1,31 @@
 ## PowerShell Profile
 
 # Vi Mode
- Set-PSReadlineOption -EditMode Vi -ViModeIndicator Cursor
+ Set-PSReadlineOption -EditMode Vi
+ Set-PSReadlineOption -ViModeIndicator Cursor
 
 # Theming
  If (Get-Command -ErrorAction SilentlyContinue Set-PoshPrompt) {
 	 Set-PoshPrompt -Theme ys
-	 Set-PSReadlineOption -ExtraPromptLineCount 1 -PromptText "% " -ContinuationPrompt "> "
+	 Set-PSReadlineOption -PromptText "% " -ContinuationPrompt "> "
  }
 
 # Aliases
- New-Alias -Name "g" -Value "git"
+ New-Alias -Name "g" -Value "git"		# Git
+ New-Alias -Name "ytdl" -Value "youtube-dl"	# Youtube-DL
+ New-Alias -Name "ff" -Value "ffmpeg"		# FFmpeg
+ New-Alias -Name "which" -Value "where.exe"	# Which
 
 # Personal modules
- if (Test-Path C:\Users\Angelo\Documents\PowerShell\personalmodules.ps1) {Import-Module C:\Users\Angelo\Documents\PowerShell\personalmodules.ps1}
+ if (Test-Path "$HOME\Documents\PowerShell\personalmodules.ps1") {Import-Module "$HOME\Documents\PowerShell\personalmodules.ps1"}
  else {Write-Host -ForegroundColor Yellow "No personalmodules.ps1 found."}
+
+ # LS, sorting by date
+ function Get-DateItem {Get-ChildItem | Sort-Object LastWriteTime -Descending}
+ New-Alias -Name "lsd" -Value "Get-DateItem"
+ # LS, name only
+ Function Get-ChildItemName {Invoke-Expression "Get-ChildItem -Name"}
+ New-Alias -Name "lsn" -Value "Get-ChildItemName"
+ # Write a note
+ Function Edit-Note {nvim "$HOME/sync/note.md"}
+ New-Alias -Name "note" -Value "Edit-Note"
