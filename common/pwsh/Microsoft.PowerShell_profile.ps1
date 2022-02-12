04271edeb24939b4e84bfe24a7644cb18956a4d3
kohnson <scp100j@gmail.com>
2022-02-11 19:33:48 -0500

    oh-my-posh no longer relies on PSGet, prompt edits

--- common/pwsh/Microsoft.PowerShell_profile.ps1
+++ common/pwsh/Microsoft.PowerShell_profile.ps1
@@ -5,21 +5,25 @@
  Set-PSReadlineOption -ViModeIndicator Cursor
 
 # Theming
- If (Get-Command -ErrorAction SilentlyContinue Set-PoshPrompt) {
-	 Set-PoshPrompt -Theme ys
-	 Set-PSReadlineOption -PromptText "% " -ContinuationPrompt "> "
+ If (Get-Command -ErrorAction SilentlyContinue oh-my-posh) {
+	 oh-my-posh --init --shell pwsh --config "~/dotfiles/common/pwsh/ys.omp.json" | Invoke-Expression
+	 If ([Security.Principal.WindowsIdentity]::GetCurrent().Groups -contains 'S-1-5-32-544') {Set-PSReadlineOption -PromptText "# " -ContinuationPrompt "> "}
+	 Else {Set-PSReadlineOption -PromptText "% " -ContinuationPrompt "> "}
  }
 
 # Aliases
  New-Alias "g" "git"			# Git
  New-Alias "ytdl" "youtube-dl"		# Youtube-DL
  New-Alias "ff" "ffmpeg"		# FFmpeg
- New-Alias "which" "where.exe"		# Which
- New-Alias "touch" "New-Item"		# Touch
- New-Alias "grep" "Select-String"	# Grep
  New-Alias "v" "nvim"			# Neovim
+ New-Alias "omp" "oh-my-posh"		# Oh-My-Posh
  Remove-Alias -Name "r"	# Remove R alias (runs last command)
- If (Get-Command soffice.com -ErrorAction SilentlyContinue) {New-Alias "libreoffice" "soffice.com"} # Libreoffice
+ If ($IsWindows) {
+	 New-Alias "lo" "soffice.com" 		# Libreoffice
+	 New-Alias "grep" "Select-String"	# Grep
+	 New-Alias "which" "where.exe"		# Which
+	 New-Alias "touch" "New-Item"		# Touch
+ }
 
 # Personal modules
  if (Test-Path "$HOME\Documents\PowerShell\personalmodules.ps1") {Import-Module "$HOME\Documents\PowerShell\personalmodules.ps1"}
@@ -38,9 +42,10 @@
  Function dry {nvim "$HOME/Documents/writing/log.rmd"}
  
 # Get public IP
- Function ip {$(Invoke-WebRequest ident.me | Select-Object Content | Select-String ":",".") -replace "@{Content=","" -replace "}",""}
+ Function ip {$(Invoke-WebRequest ident.me).Content}
 
 # Get weather
+ If ($env:WTTRlocation -eq $null) {Write-Host -ForegroundColor Yellow "`$env:WTTRlocation has not been set.`n"}
  Function wttr {Invoke-RestMethod "https://wttr.in/$env:WTTRlocation"}
 
 # Fix Posh Prompt
